3c89d21b71ef951752870a00da417e5b0f959a9f
hxtang <34997168+luckysheep861@users.noreply.github.com>
2020-05-25 13:04:38 +0800

    Add files via upload

---
+++ vim_plug_install.sh
@@ -0,0 +1,11 @@
+# vim plug
+mkdir -p ~/.vim/autoload
+wget -P ~/.vim/autoload https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# monokai
+mkdir -p ~/.vim/colors
+wget -P ~/.vim/colors https://raw.githubusercontent.com/crusoexia/vim-monokai/master/colors/monokai.vim
+
+# NDERTree
+git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
+
