3ea7dc36a6215eb42f2cabe37f54cdb6e0532796
hxtang <34997168+luckysheep861@users.noreply.github.com>
2020-06-04 19:54:59 +0800

    Update vim_plug_install.sh
    add requirements to install pyls and python-language-service

--- vim_plug_install.sh
+++ vim_plug_install.sh
@@ -1,10 +1,13 @@
+set -ex
+
+# install pyls and python-language-service
+pip install -r requirements.txt
+
 # vim plug
-mkdir -p ~/.vim/autoload
-wget -P ~/.vim/autoload https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # monokai
-mkdir -p ~/.vim/colors
-wget -P ~/.vim/colors https://raw.githubusercontent.com/crusoexia/vim-monokai/master/colors/monokai.vim
+curl -fLo ~/.vim/colors --create-dirs https://raw.githubusercontent.com/crusoexia/vim-monokai/master/colors/monokai.vim
 
 # NDERTree
 git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
