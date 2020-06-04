e278026ba063eb5e8e53584987ac57487ee22a9b
hxtang <34997168+luckysheep861@users.noreply.github.com>
2020-06-04 21:35:04 +0800

    Update install_plug.sh

--- install_plug.sh
+++ install_plug.sh
@@ -4,7 +4,7 @@ set -ex
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # monokai
-curl -fLo ~/.vim/colors --create-dirs https://raw.githubusercontent.com/crusoexia/vim-monokai/master/colors/monokai.vim
+curl -fLo ~/.vim/colors/monokai.vim --create-dirs https://raw.githubusercontent.com/crusoexia/vim-monokai/master/colors/monokai.vim
 
 # NDERTree
 git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
